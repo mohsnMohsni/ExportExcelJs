902c35468190b4fc8922f8c8cdadcf67cee4ace2
Mohsn Mohsni <mohsnmohsni.co@gmail.com>
2021-07-08 11:25:19 +0430

    commit

---
+++ exportExcelEncoded.js
@@ -0,0 +1,18 @@
+let uri = "data:application/vnd.ms-excel;base64,",
+  template = '<html><head><meta charset="UTF-8"></head><body><table>{table}</table></body></html>',
+  base64 = function (s) {
+    return window.btoa(unescape(encodeURIComponent(s)));
+  },
+  format = function (s, c) {
+    return s.replace(/{(\w+)}/g, function (m, p) {
+      return c[p];
+    });
+  };
+
+table = document.getElementById("excelData");
+let ctx = { worksheet: null || "Worksheet", table: table.innerHTML };
+
+let a = document.createElement("a");
+a.href = uri + window.btoa(unescape(encodeURIComponent("\uFEFF" + format(template, ctx))));
+a.download = `discountcode ${new Date().toLocaleDateString("fa-IR")}.xls`;
+a.click();
